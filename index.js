ff080541c9da37df5823a0b4310a21cb7d18b933
Maicol Duque <maicol.duque@qrvey.com>
2022-05-14 22:35:55 -0500

    Add CORS

--- index.js
+++ index.js
@@ -3,12 +3,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 
 const appRoutes = require('./routes')
 
 // Add body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
+
+// Set CORS
+app.use(cors({
+	origin: config.url_front, // allow to server to accept request from different origin
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	credentials: true // allow session cookie from browser to pass through
+}));
 
 //Set available routes
 app.get('/', (_req, res) => res.json({ message: 'Working!' }))
